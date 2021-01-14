03c611306321d60cf7e873a7c3e7f44c9f030baf
Radha Patel <radha.rpatel02@gmail.com>
2021-01-13 22:25:49 -0600

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://weblabwinners:weblabwinners@spotifyapp.is2qi.mongodb.net/SpotifyApp?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "SpotifyApp";
 
 // connect to mongodb
 mongoose
