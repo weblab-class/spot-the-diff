cce2582a0c85e171aee90d7c168f35cb45df1ccf
helen-hu <helenhu.pa@gmail.com>
2021-01-15 22:50:35 -0500

    replaced skeleton with login page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { get, post } from "../../utilities";
+
 
 import "../../utilities.css";
 import "./Login.css";
@@ -11,12 +13,26 @@ class Login extends Component {
         };
     }
 
+    handleLogin = () => {
+        get("/api/spotify-login").then((data) => {
+          console.log(data.url);
+          window.location.href = data.url;
+        })
+      }  
+    
+      fetchPlaylists = () => {
+        get("/api/playlists").then((data) => {
+          console.log(data);
+        })
+      }
+    
     render() {
         return (
             <div>
                 <h1>welcome! login to spotify to get started.</h1>
                 <h3>or, learn more about us first.</h3>
-                <button>Login</button>
+                <button onClick={this.handleLogin}>Login here pls</button>
+                <button onClick={this.fetchPlaylists}>Fetch playlists</button>
             </div>
         );
     }
--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
-import { get } from "../../utilities";
+import { get, post } from "../../utilities";
 
 import "../../utilities.css";
 import "./Skeleton.css";
-import { get, post } from "../../utilities";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
