1ba9c4eba7f31dd6cc6821f5b38d1d6cee1cc4bc
carolinecunningham <ccunning@mit.edu>
2021-01-15 21:15:05 -0500

    added spotify auth backend

--- server/api.js
+++ server/api.js
@@ -21,6 +21,45 @@ const router = express.Router();
 //initialize socket
 const socketManager = require("./server-socket");
 
+var SpotifyWebApi = require('spotify-web-api-node');
+scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private']
+
+require('dotenv').config();
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_API_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.CALLBACK_URL,
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// button on front end should call & query API for this func
+
+router.get('/spotify-login', (req,res) => {
+  var html = spotifyApi.createAuthorizeURL(scopes)
+  console.log(html)
+  res.send(html+"&show_dialog=true")  
+})
+
+router.get('/callback', async (req,res) => {
+  const { code } = req.query;
+  console.log(code)
+  try {
+    var data = await spotifyApi.authorizationCodeGrant(code)
+    const { access_token, refresh_token } = data.body;
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.setRefreshToken(refresh_token);
+
+    res.redirect('http://localhost:5000');
+  } catch(err) {
+    res.redirect('/#/error/invalid token');
+  }
+});
+
 router.post("/login", auth.login);
 router.post("/logout", auth.logout);
 router.get("/whoami", (req, res) => {
