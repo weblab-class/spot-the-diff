c4d7dfdac6638a474370a3a0dddbe93cfb0fdcdf
helen-hu <helenhu.pa@gmail.com>
2021-01-19 00:05:49 -0500

    deployingg plss

--- server/auth.js
+++ server/auth.js
@@ -54,7 +54,7 @@ const callback = async (req, res, spotifyApi) => {
         console.log('Something went wrong!', err);
       }).then((user) => {
         req.session.user = user;
-        res.redirect('http://localhost:5000/');
+        res.redirect(process.env.REDIRECT_URI);
       }).catch((err) => {
         console.log(`Failed to log in: ${err}`);
         res.status(401).send({ err });
